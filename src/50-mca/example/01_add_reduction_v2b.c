9252802ebf53d3d3303357c98d1731583553ffb5
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2020-05-09 01:37:41 +0200

    50: finish section about resource pressure

---
+++ src/50-mca/example/01_add_reduction_v2b.c
@@ -0,0 +1,43 @@
+//LLVM-MCA-OPTIONS -timeline -all-stats -bottleneck-analysis
+#include <stdio.h>
+#include <stdlib.h>
+#include "benchmark.h"
+
+
+#define N 5000000*8*2
+
+
+int main(int argc, char *argv[])
+{
+  double *buffer = mallocBufferDouble(N, 0.0, 1.0);
+
+  benchmark_state_t bench;
+  BENCHMARK_BEGIN(bench, "+ accumulator", 100, 2);
+  
+  double accum = 0.0;
+  
+  BENCHMARK_TIMER_START(bench);
+  
+  for (int i=0; i<N; i+=8) {
+    __asm volatile("# LLVM-MCA-BEGIN plus_accum_body");
+    accum += buffer[0];
+    accum += buffer[0];
+    accum += buffer[0];
+    accum += buffer[0];
+    accum += buffer[0];
+    accum += buffer[0];
+    accum += buffer[0];
+    accum += buffer[0];
+    __asm volatile("# LLVM-MCA-END");
+  }
+  
+  BENCHMARK_TIMER_STOP(bench);
+  
+  printf("result = %lf\n", accum); // approx 40000000
+  BENCHMARK_END(bench);
+  
+  free(buffer);
+  return 0;
+}
+
+
